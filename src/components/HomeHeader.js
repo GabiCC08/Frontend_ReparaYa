8e1679048c5417f7736543a08bbf7b2de8ec807a
jonathan141999 <jona94alquinga@gmail.com>
2020-09-03 22:51:18 -0500

    Login sin Funciones

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -8,13 +8,17 @@ const {Header} = Layout;
 const HomeHeader = ()=> {
 
     const [web, setWeb] = useState(false);
-    const [ createInfoModalVisible, setCreateInfoModalVisible ] = useState( false );
+    const [ok, setOk] = useState(false);
+    const [cancel, setCancel] = useState(false);
 
-    const handleModal = () => {
-        setCreateInfoModalVisible( true );
-    }
     const showModal = () => {
         setWeb(true);
+    }
+    const handleOk = () =>{
+        setOk(true)
+    }
+    const handleCancel = () =>{
+        setCancel(true)
     }
     return (
          <Header>
@@ -24,40 +28,35 @@ const HomeHeader = ()=> {
                 </Col>
                 <Col>
                     <Button>UNIRSE AHORA</Button>
-                    <Button type="primary" onClick={showModal}>INICIAR SESIÓN</Button>
-
+                    <Button type="primary" onClick={showModal}>
+                        INICIAR SESIÓN
+                    </Button>
                     <Modal
+                        title="BIENVENIDOS"
                         visible={web}
+                        onOk={false}
+                        onCancel={false}
+                        footer={null}
                     >
-                        <div className="ant-modal-content">
-                            <Card>
-                                <Form name="basic" initialValues={{remember: true}}>
-                                    <Form.Item name="email" rules={[{ type: 'email',required: true, message: 'Por favor, ingresa tu email.' }]}>
-                                        <Input placeholder="Email"/>
-                                    </Form.Item>
+                        <Form name="basic" initialValues={{remember: true}}>
+                            <Form.Item name="email" rules={[{ type: 'email',required: true, message: 'Por favor, ingresa tu email.' }]}>
+                                <Input placeholder="Email"/>
+                            </Form.Item>
 
-                                    <Form.Item name="password" rules={[{required: true, message: 'Por favor, ingresa tu clave.'}]}>
-                                        <Input.Password placeholder="Contraseña"/>
-                                    </Form.Item>
+                            <Form.Item name="password" rules={[{required: true, message: 'Por favor, ingresa tu clave.'}]}>
+                                <Input.Password placeholder="Contraseña"/>
+                            </Form.Item>
 
-                                    <Form.Item name="remember" valuePropName="checked">
-                                        <Checkbox>Recordarme</Checkbox>
-                                    </Form.Item>
-
-                                    <Form.Item>
-                                        <Button type="primary" htmlType="submit" onClick={handleModal}>
-                                            INGRESAR
-                                        </Button>
-                                    </Form.Item>
-
-                                    <Form.Item>
-                                        <Button onClick={handleModal}>
-                                            CREAR CUENTA
-                                        </Button>
-                                    </Form.Item>
-                                </Form>
-                            </Card>
-                        </div>
+                            <Form.Item name="remember" valuePropName="checked">
+                                <Checkbox>Recordarme</Checkbox>
+                            </Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                    INGRESAR
+                            </Button>
+                            <Button>
+                                    CREAR CUENTA
+                            </Button>
+                        </Form>
                     </Modal>
                 </Col>
 
