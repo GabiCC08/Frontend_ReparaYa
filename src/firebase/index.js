e0bd9fd6764da39b4dc373a411eb00634b292db7
Gabriela <gabriela.cola@epn.edu.ec>
2020-09-20 10:42:44 -0500

    Formularios

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,10 @@
 import app from 'firebase/app'
 import 'firebase/database';
+import 'firebase/auth';
 
 console.log('ENV', process.env.NODE_ENV)
 
-const config= {
+const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -13,7 +14,13 @@ const config= {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
- app.initializeApp(config);
+app.initializeApp(config);
 
- export const db= app.database();
+export const db = app.database();
+
+const FIREBASE = {
+    db
+}
+
+export default FIREBASE;
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,133 +1,140 @@
 import React from 'react';
 import SimpleHeader from "../components/SimpleHeader";
-import {Button, Form, Input} from "antd";
+import {Button, Form, Input, message,Switch} from "antd";
 import Routes from "../constants/routes";
-import {Link, useLocation} from "react-router-dom";
+import {Link} from "react-router-dom";
 import '../styles/Register.less';
-import { LockOutlined, UserOutlined, MailOutlined, EditOutlined, FileTextOutlined } from '@ant-design/icons';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons/lib';
+import FIREBASE from "../firebase";
 
 
+const Register = () => {
 
-const Register = () => (
-    <div>
-        <SimpleHeader/>
-        <div className='section' id='imgRegister'>
-            <h1 id='hFrm' style={{textAlign: 'Center',fontsize:'10px'}}>REGISTRATE EN REPARA YA </h1>
-            <div id='register'>
-            <Form name='register-form'
-                  className='register-form'
-                  initialValues={ {
-                      email: '',
-                      password: ''
-                  } }
+    const handleSubmit= async (values)=>{
+        console.log('datos de la base de Datos')
+        await FIREBASE.db.ref('user').push(values);
+        message.success('Los datos se Guardaron Correctamente');
+    }
+    return(
+        <div>
+            <SimpleHeader/>
+            <div className='section' id='imgRegister'>
+                <h1 id='hFrm' style={{textAlign: 'Center',fontsize:'10px'}}>REGISTRATE EN REPARA YA </h1>
+                <div id='register'>
+                    <Form name='register-form'
+                          className='register-form'
+                          initialValues={ {
+                              email: '',
+                              password: ''
+                          } }
 
-            >
-                <Form.Item name='name'
-                           rules={ [
-                               {
-                                   required: true,
-                                   message: 'Ingresa tu nombre'
-                               }
-                           ] }
-                           hasFeedback
-                >
-                    <Input prefix={ <UserOutlined /> } placeholder='Nombre' />
-                </Form.Item>
+                    >
+                        <Form.Item name={['user', 'name']} label="Nombres Completos"
+                                   rules={[{required: true, message: 'Por favor, ingresa tus nombres completo.'}]} hasFeedback>
+                            <Input placeholder='Nombres' />
+                        </Form.Item>
 
-                <Form.Item name='email'
-                           rules={ [
-                               {
-                                   required: true,
-                                   message: 'Ingresa tu nombre de usuario'
-                               },
-                               {
-                                   type: 'email',
-                                   message: 'Ingresa un correo válido'
-                               }
-                           ] }
-                           hasFeedback
-                >
-                    <Input prefix={ <MailOutlined /> } placeholder='Email' />
-                </Form.Item>
+                        <Form.Item name={['user', 'lastname']} label="Apellidos Completos"
+                                   rules={[{required: true, message: 'Por favor, tus apellidos completos'}]} hasFeedback>
+                            <Input placeholder='Apellidos' />
+                        </Form.Item>
 
-                <Form.Item name='password'
-                           rules={ [
-                               {
-                                   required: true,
-                                   message: 'Ingresa tu clave'
-                               }
-                           ] }
-                           hasFeedback
-                >
-                    <Input.Password prefix={ <LockOutlined /> }
-                                    iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                                    placeholder='Clave' />
-                </Form.Item>
-
-                <Form.Item name='password_confirmation'
-                           dependencies={ [ 'password' ] }
-                           hasFeedback
-                           rules={ [
-                               {
-                                   required: true,
-                                   message: 'Confirma tu clave',
-                               },
-                               ( { getFieldValue } ) => ({
-                                   validator( rule, value ) {
-                                       if( !value || getFieldValue( 'password' ) === value ) {
-                                           return Promise.resolve();
+                        <Form.Item name={['user', 'email']} label="Email"
+                                   rules={ [
+                                       {
+                                           required: true,
+                                           message: 'Ingresa tu nombre de usuario'
+                                       },
+                                       {
+                                           type: 'email',
+                                           message: 'Ingresa un correo válido'
                                        }
-                                       return Promise.reject( 'Las claves no coinciden' );
-                                   },
-                               }),
-                           ] }
-                >
-                    <Input.Password prefix={ <LockOutlined /> }
-                                    iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                                    placeholder='Confirma tu clave' />
-                </Form.Item>
+                                   ] }
+                                   hasFeedback
+                        >
+                            <Input placeholder='Email' />
+                        </Form.Item>
 
-                <Form.Item name='Profesion'
-                           rules={ [
-                               {
-                                   required: true,
-                                   message: 'Ingresas a que te dedicas'
-                               }
-                           ] }
-                           hasFeedback
-                >
-                    <Input prefix={ <EditOutlined /> } placeholder='Ciudad' />
-                </Form.Item>
+                        <Form.Item name={['user', 'password']} label="Contraseña"
+                                   rules={ [
+                                       {
+                                           required: true,
+                                           message: 'Ingresa tu clave'
+                                       }
+                                   ] }
+                                   hasFeedback
+                        >
+                            <Input.Password
+                                iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                                placeholder='Clave' />
+                        </Form.Item>
 
-                <Form.Item name='short_bio'
-                           rules={ [
-                               {
-                                   required: true,
-                                   message: 'Cuéntanos un poco sobre ti.'
-                               }
-                           ] }
-                           hasFeedback
-                >
-                    <Input.TextArea prefix={ <FileTextOutlined /> }
-                                    placeholder='Cuéntanos un poco de  ti'
-                                    autoSize={ {
-                                        minRows: 2,
-                                        maxRows: 6
-                                    } } />
-                </Form.Item>
+                        <Form.Item name={['user', 'password-confirmation']} label="Contraseña"
+                                   dependencies={ [ 'password' ] }
+                                   hasFeedback
+                                   rules={ [
+                                       {
+                                           required: true,
+                                           message: 'Confirma tu clave',
+                                       },
+                                       ( { getFieldValue } ) => ({
+                                           validator( rule, value ) {
+                                               if( !value || getFieldValue( 'password' ) === value ) {
+                                                   return Promise.resolve();
+                                               }
+                                               return Promise.reject( 'Las claves no coinciden' );
+                                           },
+                                       }),
+                                   ] }
+                        >
+                            <Input.Password
+                                iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                                placeholder='Vuelve a escribir la Clave' />
+                        </Form.Item>
 
-                <Form.Item>
-                    <Link to={Routes.PROFILE}><Button type='primary' htmlType='submit' className='login-form-button'>
-                        Registrarme
-                    </Button></Link>
-                    <Link to={Routes.HOME}><Button type="default">Ya tengo una Cuenta</Button></Link>
-                </Form.Item>
-            </Form>
+                        <Form.Item name={['user', 'visible']}  label="Visible" rules={ [
+                            {
+                                required: true,
+                            }
+                        ] }>
+                            <Switch />
+                        </Form.Item>
 
+                        <Form.Item name={['user', 'city']} label="Ciudad"
+                                   rules={ [
+                                       {
+                                           required: true,
+                                           message: 'Ingresas tu ciudad'
+                                       }
+                                   ] }
+                                   hasFeedback
+                        >
+                            <Input  placeholder='Ciudad' />
+                        </Form.Item>
+                        <Form.Item name={['user', 'job-type']} label="Trabajo"
+                                   rules={ [
+                                       {
+                                           required: true,
+                                           message: 'Ingresas tu ciudad'
+                                       }
+                                   ] }
+                                   hasFeedback
+                        >
+                            <Input  placeholder='Ciudad' />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Link to={Routes.PROFILE}><Button type='primary' htmlType='submit' className='login-form-button'>
+                                Registrarme
+                            </Button></Link>
+                            <Link to={Routes.HOME}><Button type="default">Ya tengo una Cuenta</Button></Link>
+                        </Form.Item>
+                    </Form>
+
+                </div>
             </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Register;
