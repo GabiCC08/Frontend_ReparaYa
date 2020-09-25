6ade6d981a8fea94dae6ed1f7c6235bb1674ee27
Gabriela <gabriela.cola@epn.edu.ec>
2020-09-25 07:06:57 -0500

    Correccion form publicacion

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -5,7 +5,6 @@ import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 import Routes from "../constants/routes";
 import {Link} from "react-router-dom";
 import '../styles/Publications.less'
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons/lib';
 import FIREBASE from "../firebase";
 
 
@@ -34,90 +33,103 @@ const Publications = () => {
     return(
         <>
             <SimpleHeader/>
-            <div className="main-publications" id='publications'>
-                <h1 id='hFrm' style={{textAlign: 'Center',fontsize:'10px'}}>Crea una publicación</h1>
+            <div className="main-publications" id='publicationForm'>
+                <h1 className='hFrm' style={{textAlign: 'Center',fontsize:'10px'}}>Crea una publicación</h1>
                 <Form
                     name='publications-form'
                     onFinish={handleSubmit}
                 >
                     <Row>
                         <Col span={12} style={{margin: 'auto'}}>
-                            <Form.Item name={['tittle']} label="Titulo de la publicación"
-                                       rules={[{required: true, message: 'Por favor, ingresa un titulo para la publicacion.'}]}>
-                                <Input/>
-                            </Form.Item>
-
-                            <Form.Item
-                                label='Ciudad'
-                                name='city'
-                                rules={ [
-                                    {
-                                        required: true,
-                                        message: 'Especifique el Sector'
-                                    }
-                                ]}
-                            >
-                                <Select>
-                                    <Select.Option value="quito">Quito</Select.Option>
-                                    <Select.Option value="guayaquil">Guayaquil</Select.Option>
-                                    <Select.Option value="cuenca">Cuenca</Select.Option>
-                                    <Select.Option value="ambato">Ambayo</Select.Option>
-                                    <Select.Option value="imbabura">Imbabura</Select.Option>
-                                    <Select.Option value="machala">Machala</Select.Option>
-                                </Select>
-                            </Form.Item>
-
-                            <Form.Item
-                                label='Area Laboral'
-                                name='job_type'
-                                rules={ [
-                                    {
-                                        required: true,
-                                        message: 'Escoga el area de trabajo'
-                                    }
-                                ]}
-                            >
-                                <Select>
-                                    <Select.Option value="mecanico">Mecanico</Select.Option>
-                                    <Select.Option value="electricista">Electricista</Select.Option>
-                                    <Select.Option value="carpinteria">Carpinteria</Select.Option>
-                                    <Select.Option value="plomeria">Plomeria</Select.Option>
-                                    <Select.Option value="electrodocmesticos">Electrodomesticos</Select.Option>
-                                </Select>
-                            </Form.Item>
-
-                            <Form.Item
-                                label='Detalla tu requerimiento/ oferta laboral: '
-                                name='descripcion'
-                                rules={ [
-                                    {
-                                        required: true,
-                                        message: 'Porfavor ingresa una pequeña descripcion'
-                                    }
-                                ]}
-                            >
-                                <Input.TextArea/>
-                            </Form.Item>
-
-                            <Form.Item label="Foto:">
-                                <Form.Item name="photo" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
-                                    <Upload.Dragger name="files" action="/upload.do">
-                                        <p className="ant-upload-drag-icon">
-                                            <InboxOutlined />
-                                        </p>
-                                        <p className="ant-upload-text">Click para subir una foto</p>
-                                    </Upload.Dragger>
+                            <Form>
+                                <Form.Item name={['tittle']} label="Titulo de la publicación"
+                                           rules={[{required: true, message: 'Por favor, ingresa un titulo para la publicacion.'}]}>
+                                    <Input/>
                                 </Form.Item>
-                            </Form.Item>
 
-                            <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-                                <Button  type="primary" htmlType="submit" style={{float: 'rigth'}}>
-                                    Registrar
-                                </Button>
-                                <Link to={Routes.PROFILE}><Button>
-                                    Regresar
-                                </Button></Link>
-                            </Form.Item>
+                                <Form.Item
+                                    label='Ciudad'
+                                    name='city'
+                                    rules={ [
+                                        {
+                                            required: true,
+                                            message: 'Especifique el Sector'
+                                        }
+                                    ]}
+                                >
+                                    <Select className="opcSelect">
+                                        <Select.Option value="Ambato">Ambato</Select.Option>
+                                        <Select.Option value="Cuenca">Cuenca</Select.Option>
+                                        <Select.Option value="Duran">Duran</Select.Option>
+                                        <Select.Option value="Esmeraldas">Esmeraldas</Select.Option>
+                                        <Select.Option value="Guayaquil">Guayaquil</Select.Option>
+                                        <Select.Option value="Loja">Loja</Select.Option>
+                                        <Select.Option value="Machala">Machala</Select.Option>
+                                        <Select.Option value="Manta">Manta</Select.Option>
+                                        <Select.Option value="Portoviejo">Portoviejo</Select.Option>
+                                        <Select.Option value="Quito">Quito</Select.Option>
+                                        <Select.Option value="Santo Domingo">Santo Domingo</Select.Option>
+                                    </Select>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label='Area Laboral'
+                                    name='job_type'
+                                    rules={ [
+                                        {
+                                            required: true,
+                                            message: 'Escoga el area de trabajo'
+                                        }
+                                    ]}
+                                >
+                                    <Select className="opcSelect">
+                                        <Select.Option value="Albañileria">Albañilería</Select.Option>
+                                        <Select.Option value="Carpinteria">Carpintería</Select.Option>
+                                        <Select.Option value="Cerrajeria">Cerrajería</Select.Option>
+                                        <Select.Option value="Electricidad">Electricidad</Select.Option>
+                                        <Select.Option value="Electrodomésticos">Electrodomésticos</Select.Option>
+                                        <Select.Option value="Electronica">Electrónica</Select.Option>
+                                        <Select.Option value="Fontaneria">Fontanería</Select.Option>
+                                        <Select.Option value="Mecánica">Mecánica</Select.Option>
+                                        <Select.Option value="Pintura">Pintura</Select.Option>
+                                        <Select.Option value="Radiotecnia">Radiotécnia</Select.Option>
+                                        <Select.Option value="Soldadura">Soldadura</Select.Option>
+                                    </Select>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label='Detalla tu requerimiento/ oferta laboral: '
+                                    name='descripcion'
+                                    rules={ [
+                                        {
+                                            required: true,
+                                            message: 'Porfavor ingresa una pequeña descripcion'
+                                        }
+                                    ]}
+                                >
+                                    <Input.TextArea/>
+                                </Form.Item>
+
+                                <Form.Item label="Foto:">
+                                    <Form.Item name="photo" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                                        <Upload.Dragger name="files" action="/upload.do">
+                                            <p className="ant-upload-drag-icon">
+                                                <InboxOutlined />
+                                            </p>
+                                            <p className="ant-upload-text">Click para subir una foto</p>
+                                        </Upload.Dragger>
+                                    </Form.Item>
+                                </Form.Item>
+
+                                <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+                                    <Button  type="primary" htmlType="submit" style={{float: 'rigth'}}>
+                                        Registrar
+                                    </Button>
+                                    <Link to={Routes.PROFILE}><Button>
+                                        Regresar
+                                    </Button></Link>
+                                </Form.Item>
+                            </Form>
 
                         </Col>
                     </Row>
